9d875561e1e02d41d70564dea6cfe50ae3e70a35
Tcholly <roberti.gionata@protonmail.ch>
2023-04-10 19:08:04 +0200

    Added lerpcolor with multiple colors

--- Difu/Difu/Utils/MathExtension.h
+++ Difu/Difu/Utils/MathExtension.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #if !defined(RL_VECTOR2_TYPE)
 // Vector2 type
 typedef struct Vector2
@@ -130,11 +132,13 @@ inline void operator/=(Vector2& a, const float value)
 // -----------------------------------------------------------------------------------------------------------------
 // Rectangle Functions ---------------------------------------------------------------------------------------------
 
+// Get the position of the rectangle
 inline Vector2 GetRectanglePosition(const Rectangle& rectangle)
 {
     return { rectangle.x, rectangle.y };
 }
 
+// Get the size of the rectangle
 inline Vector2 GetRectangleSize(const Rectangle& rectangle)
 {
     return { rectangle.width, rectangle.height };
@@ -143,6 +147,7 @@ inline Vector2 GetRectangleSize(const Rectangle& rectangle)
 // -----------------------------------------------------------------------------------------------------------------
 // Color Functions -------------------------------------------------------------------------------------------------
 
+// Lerps from one color to another depending on progress
 inline Color LerpColor(Color start, Color end, float progress)
 {
     int r = start.r + progress * (end.r - start.r);
@@ -150,4 +155,14 @@ inline Color LerpColor(Color start, Color end, float progress)
     int b = start.b + progress * (end.b - start.b);
     int a = start.a + progress * (end.a - start.a);
     return {(unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)a};
+}
+
+// Lerp from each color to the next depending on progress, where 0.0f is the first color and 1.0f is the last, while going trough each color
+inline Color LerpColor(std::vector<Color> colors, float progress)
+{
+	int n_colors = colors.size();
+	int index_from = (int)(progress * (n_colors - 1));
+	if (index_from > n_colors - 2)
+		return colors[n_colors - 1];
+	return LerpColor(colors[index_from], colors[index_from + 1], (progress - (1.0f * index_from) / (n_colors - 1)) * (n_colors - 1));
 }
